504362aa5b5101ad3cd2e0a708bb2b62497b67c2
root <root@chef-workstation.dexter.com.br>
2019-10-31 21:07:02 -0200

    config

---
+++ recipes/metadata.rb
@@ -0,0 +1,9 @@
+name 'confsbase-MarcioBelliniDO'
+maintainer 'Marcio Bellini'
+maintainer_email 'marcio.bellini@gmail.com'
+license 'All Rights Reserved'
+description 'Configuracao base'
+long_description 'Configuracao base de usuarios, pacotes e hosts'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url "https://github.com/MarcioBelliniDO/confsbase-MarcioBelliniDO"
